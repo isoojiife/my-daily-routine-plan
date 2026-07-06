afbe65c77e90915197c75b6e501af125890f20fd
isoojiife <adeade742@gmail.com>
2026-07-06 18:22:24 +0100

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,12 @@
+#activity1
+name = "isojiife"
+age = 11
+print("--- Running Operations ---")
+clean_name = name.capitalize()
+print("Name: " + clean_name)
+print("Age: " + str(age))
+name_length = len(name)
+print("Letters in your name: " + str(name_length))
+print("Story: My name is " + clean_name + " and I am " + str(age) + " years old.")
+next_year = age + 1
+print("Next year I will be: " + str(next_year))
